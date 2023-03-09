fd3599a5d9148a7f96db3d9440fc3429d696d89c
Ilinca <you@example.com>
2023-03-09 12:20:26 +0200

    init

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+//declare the structure for a letter in the word
+//we will assume we have 64x128px display
+typedef struct letter
+{
+    int X_POS;
+    int Y_POS;
+    bool ENABLE;
+    int SIZE;
+};
+int main(void)
+{
+    return 0;
+};
